fc85f34ebfa98ce07104f7caf237e716a6394cd0
Jack Fong <godkin.mo@gmail.com>
2021-10-16 16:32:19 +0800

    wip

--- main.js
+++ main.js
@@ -1,26 +1,77 @@
-const btnEl = document.querySelector('#open-btn')
+const menuBtnEl = document.querySelector('#menu-btn')
+const menuListEl = document.querySelector('#menu-list')
+let menuOpened = false
 
-const modalEl = document.querySelector('#modal')
+menuBtnEl.addEventListener('click', () => {
+  toogleMenu()
+})
 
-btnEl.addEventListener('click', () => openModal())
+function toogleMenu() {
+  console.log('menuopened', menuOpened)
+  menuOpened = !menuOpened
 
-modalEl.querySelector('div:nth-child(1)').addEventListener('click', () => closeModal())
+  menuBtnEl.querySelector('svg:nth-child(1)').classList.toggle('hidden')
+  menuBtnEl.querySelector('svg:nth-child(2)').classList.toggle('hidden')
 
-function openModal() {
-  modalEl.classList.remove('hidden')
-  modalEl.classList.add('flex')
-  modalEl.classList.add('fade-in')
-  modalEl.classList.remove('fade-out')
-  document.querySelector('body').classList.add('overflow-hidden')
+  menuListEl.classList.toggle('-translate-x-full')
 }
 
-function closeModal() {
-  modalEl.classList.add('fade-out')
-  modalEl.classList.remove('fade-in')
-  document.querySelector('body').classList.remove('overflow-hidden')
+// toogleMenu()
 
-  setTimeout(() => {
-    modalEl.classList.add('hidden')
-    modalEl.classList.remove('flex')
-  }, 300)
-}
+// import axios from 'axios'
+
+// axios.get('https://rickandmortyapi.com/api/character', {
+//   params: {
+//     page: 2,
+//   }
+// })
+//   .then(({ status, data }) => {
+//     console.log('data', status, data)
+//   })
+
+// function getUserInfo() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(({ name: 'Jack' })), 300)
+//   })
+// }
+
+// async function run () {
+//   const [data1, _, posts] = await Promise.all([
+//     getUserInfo(),
+//     new Promise(resolve => setTimeout(() => resolve(), 1000)),
+//     axios.get('./data/posts.json')
+//   ])
+
+//   console.log('Done!', posts)
+// }
+
+// window.run = run
+
+
+
+// const btnEl = document.querySelector('#open-btn')
+
+// const modalEl = document.querySelector('#modal')
+
+// btnEl.addEventListener('click', () => openModal())
+
+// modalEl.querySelector('div:nth-child(1)').addEventListener('click', () => closeModal())
+
+// function openModal() {
+//   modalEl.classList.remove('hidden')
+//   modalEl.classList.add('flex')
+//   modalEl.classList.add('fade-in')
+//   modalEl.classList.remove('fade-out')
+//   document.querySelector('body').classList.add('overflow-hidden')
+// }
+
+// function closeModal() {
+//   modalEl.classList.add('fade-out')
+//   modalEl.classList.remove('fade-in')
+//   document.querySelector('body').classList.remove('overflow-hidden')
+
+//   setTimeout(() => {
+//     modalEl.classList.add('hidden')
+//     modalEl.classList.remove('flex')
+//   }, 300)
+// }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  mode: 'jit',
+  // mode: 'jit',
   purge: [
     'index.html'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      fontFamily: {
+        sans: [...defaultTheme.fontFamily.sans],
+      },
       colors: {
         'brand': '#00A4EF',
       },
